7606391dbc4f3c9521f279ca1a84eb27e8a831d8
Maxson <you@example.com>
2023-09-27 11:07:41 +0300

    0.1.1 - added episodes (test version)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,22 @@
 import './Header.css';
 import {useState} from "react";
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import {Scrollbar} from "swiper/modules";
+
+import 'swiper/css';
+import 'swiper/css/scrollbar';
 function Header() {
 
     const [isHovered, setIsHovered] = useState(false);
 
-    const handleDonateHover = () => {
-        setIsHovered(true);
-    };
+    const handleDonateHover = () => {setIsHovered(true);};
+    const handleDonateLeave = () => {setIsHovered(false);};
 
-    const handleDonateLeave = () => {
-        setIsHovered(false);
+
+    const [isEpisodesMenuOpen, setIsEpisodesMenuOpen] = useState(false);
+    const toggleEpisodesMenu = () => {
+        setIsEpisodesMenuOpen(!isEpisodesMenuOpen);
     };
 
     return (
@@ -18,19 +24,39 @@ function Header() {
             <div className="header-border">
                 <a href="/" className="logo">Castpress.</a>
                 <nav className="menubar">
-                    <div>
-                        <a id="episodes" href="/episodes">
+                    <div className="menubar__button">
+                        <a id="episodes" href="#" onClick={toggleEpisodesMenu}>
                             EPISODES
-                            <span className="icon-simple-arrow-bottom"></span>
+                            <span className={isEpisodesMenuOpen ? "simple-arrow icon-simple-arrow-bottom" : "simple-arrow icon-simple-arrow-top"}></span>
                         </a>
                     </div>
-                    <div>
+                    {isEpisodesMenuOpen && (
+                        <Swiper className="episodes-menu"
+                                direction={"vertical"}
+                                modules={[Scrollbar]}
+                                spaceBetween={8}
+                                slidesPerView={3.8}
+                                scrollbar={{ draggable: true }}
+                                mousewheel={{ invert: false , forceToAxis: true }} /* Додано налаштування прокрутки миші */
+                                freeMode={true} /* Додано для включення режиму вільної прокрутки */
+                        >
+                            <SwiperSlide><a href="/episode_1">Episode 1</a></SwiperSlide>
+                            <SwiperSlide><a href="/episode_2">Episode 2</a></SwiperSlide>
+                            <SwiperSlide><a href="/episode_3">Episode 3</a></SwiperSlide>
+                            <SwiperSlide><a href="/episode_4">Episode 4</a></SwiperSlide>
+                            <SwiperSlide><a href="/episode_5">Episode 5</a></SwiperSlide>
+                            <SwiperSlide><a href="/episode_6">Episode 6</a></SwiperSlide>
+                            <SwiperSlide><a href="/episode_7">Episode 7</a></SwiperSlide>
+                            <SwiperSlide><a href="/episode_8">Episode 8</a></SwiperSlide>
+                        </Swiper>
+                    )}
+                    <div className="menubar__button">
                         <a href="/blog">
                             BLOG
                         </a>
                     </div>
-                    <div><a href="/contact">CONTACT</a></div>
-                    <div>
+                    <div className="menubar__button"><a href="/contact">CONTACT</a></div>
+                    <div className="menubar__button">
                         <a id="donate" href="/donate"
                            onMouseEnter={handleDonateHover}
                            onMouseLeave={handleDonateLeave}
@@ -39,7 +65,7 @@ function Header() {
                             DONATE
                         </a>
                     </div>
-                    <div><span className="icon-search"></span></div>
+                    <div className="menubar__button"><span className="icon-search"></span></div>
                 </nav>
             </div>
             <span className="decor"></span>
