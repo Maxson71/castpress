854e4b1703de641c2f179f1e60fdd04d29b4cbf0
Maxson <you@example.com>
2023-10-04 21:21:46 +0300

    v1

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -6,17 +6,22 @@ const download__icon = "../icons/download-icon.jpg"
 const play__icon = "../icons/play-icon.jpg"
 const pause__icon = "../icons/pause-icon.jpg"
 const volume__icon = "../icons/volume-icon.jpg"
+const volume_off__icon = "../icons/volume-off-icon.jpg"
 
-const curent__image = "../img/curent-episode.jpg"
-const curent__audio = "../audio/file_example_MP3_5MG.mp3"
+const current__episode = "How to rapidly test any experience!"
+const current__image = "../img/current-episode.jpg"
+const current__audio = "../audio/file_example_MP3_5MG.mp3"
 
 function AudioPlayer() {
 
     const [isPlaying, setIsPlaying] = useState(false);
+    const [isOpenVolume, setIsOpenVolume] = useState(false);
     const [currentTime, setCurrentTime] = useState(0);
+    const [currentVolume, setCurrentVolume] = useState(0.5);
     const [duration, setDuration] = useState(0);
     const audioRef = useRef(null);
-    let progressBarWidth = `${(currentTime / duration) * 100}%`;
+    let audioProgressBarWidth = `${(currentTime / duration) * 100}%`;
+    let volumeProgressBarWidth = `${(currentVolume*100)}%`;
 
     useEffect(() => {
         const onLoadedMetadata = () => {
@@ -54,17 +59,35 @@ function AudioPlayer() {
         return `${minutes}:${seconds}`;
     };
 
+    const volumeChangeHandler = (e) => {
+        const newVolume = parseFloat(e.target.value);
+        audioRef.current.volume = newVolume;
+        setCurrentVolume(newVolume);
+    };
+
+    const VolumeHandler = () => {
+        setIsOpenVolume(!isOpenVolume);
+    };
+
+    const downloadAudioHandler = () => {
+        const a = document.createElement('a');
+        a.href = current__audio;
+        a.download = 'audio.mp3';
+        a.click();
+    };
+
     return (
-        <div className="curent-episode">
-            <img src={curent__image} alt="" className="curent-episode__image"/>
-            <h1 className="curent-episode__title">How to rapidly test any experience!</h1>
-            <div className="curent-episode__audio">
+        <div className="current-episode">
+            <img src={current__image} alt="" className="current-episode__image"/>
+            <h1 className="current-episode__title">{current__episode}</h1>
+            <div className="current-episode__audio">
                 <div className="audio-play-button" onClick={playPauseHandler}>
                     <img className="audio-button__image" src={isPlaying ? play__icon : pause__icon} alt="Play/Pause" />
                 </div>
-                <h6 className="audio-curent-time">{formatTime(currentTime)}</h6>
+                <h6 className="audio-current-time">{formatTime(currentTime)}</h6>
                 <div className="audio-progress-bar">
                     <input
+                        className="audio-progress-bar__input"
                         type="range"
                         min="0"
                         max={Math.floor(duration)}
@@ -74,23 +97,38 @@ function AudioPlayer() {
                             audioRef.current.currentTime = newTime;
                             setCurrentTime(newTime);
                         }}
-                        style={{ '--progress-width': progressBarWidth }}
+                        style={{ '--progress-audio-width': audioProgressBarWidth }}
                     />
                 </div>
-                <h6 className="audio-full-time">{formatTime(duration)}</h6>
-                <div className="audio-volume-button">
-                    <img className="audio-button__image" src={volume__icon} alt="Volume" />
+                <h6 className="audio-full-time">{formatTime(duration-currentTime)}</h6>
+                <div className="audio-volume-button" onClick={VolumeHandler}>
+                    <img className="audio-button__image" src={(currentVolume == 0)? volume_off__icon : volume__icon} alt="Volume" />
                 </div>
-                <div className="audio-download-button">
+                <div className={isOpenVolume ? "volume-progress-bar volume-open" : "volume-progress-bar volume-close" }>
+                    <input
+                        className="volume-progress-bar__input"
+                        type="range"
+                        min="0"
+                        max="1"
+                        step="0.01"
+                        value={currentVolume}
+                        onChange={volumeChangeHandler}
+                        style={{ '--progress-volume-width': volumeProgressBarWidth }}
+                    />
+                </div>
+                <div className="audio-download-button" onClick={downloadAudioHandler}>
                     <img className="audio-button__image" src={download__icon} alt="Download" />
                 </div>
             </div>
-            <h6 className="curent-episode__link-title">LISTEN ON</h6>
-            <div className="curent-episode__link">
+            <h6 className="current-episode__link-title">LISTEN ON</h6>
+            <div className="current-episode__link">
+                <a href="https://open.spotify.com/" target="_blank" className="icon-spotify"></a>
+                <a href="https://soundcloud.com/" target="_blank" className="icon-soundcloud"></a>
+                <a href="https://music.apple.com/" target="_blank" className="icon-apple"></a>
             </div>
             <audio
                 ref={audioRef}
-                src={curent__audio}
+                src={current__audio}
                 onTimeUpdate={timeUpdateHandler}
                 onLoadedMetadata={(e) => setCurrentTime(e.target.duration)}
             />
