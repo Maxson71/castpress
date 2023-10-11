43c14436f05d76e5fee73d45074621043ff7cab2
Maxson <you@example.com>
2023-10-11 13:02:06 +0300

    fix home-page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,6 +6,26 @@ function Home() {
     return (
         <main className="home-page border">
             <AudioPlayer/>
+            <div className="latest-posts">
+                <h1 className="latest-posts__title">
+                    Latest Posts
+                </h1>
+                <div className="latest-posts__list">
+                    <div className="latest-posts__item">
+                        <img src="" alt="" className="latest-posts__img"/>
+                        <div className="latest-posts__info">
+                            <h2 className="latest-posts__title"></h2>
+                            <h5 className="latest-posts__date"></h5>
+                            <h5 className="latest-posts__more"></h5>
+                        </div>
+                    </div>
+                </div>
+                <div className="blog__button">
+                    <h5>
+                        View Blog
+                    </h5>
+                </div>
+            </div>
         </main>
     );
 }
