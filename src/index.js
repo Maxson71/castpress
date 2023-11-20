f3656b0734a608baad7687ac3c3898cf6bcdbbe4
Maxson <you@example.com>
2023-11-20 18:37:19 +0200

    fix github page

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {BrowserRouter} from "react-router-dom";
 import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
+    <BrowserRouter basename="https://Maxson71.github.io/castpress/">
         <App/>
     </BrowserRouter>
 );
